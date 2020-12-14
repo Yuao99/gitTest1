72d24cb5b45a3e965ffb2fc67a2eda2f9533dece
yuaf <yafowo@qq.com>
2020-12-14 22:29:41 +0800

    夺命三仙剑

---
+++ src/main/java/huanshan/SanXianJian.java
@@ -0,0 +1,10 @@
+package huanshan;
+
+public class SanXianJian {
+    public static void main(String[] args) {
+        System.out.println("夺命散仙剑");
+        System.out.println("夺命散仙剑");
+        System.out.println("夺命散仙剑");
+        System.out.println("夺命散仙剑");
+    }
+}
